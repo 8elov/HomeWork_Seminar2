da70cddac788602c870a9ffade94ff4a32d50879
Nikita Belov <lolvvto@yandex.ru>
2022-10-22 17:12:29 +0700

    Add App 1

---
+++ App_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+int a1 = number / 100;
+int a2 = number % 100 / 10;
+int a3 = number % 10;
+
+Console.WriteLine("Вторая цифра числа: " + a2);
+}
+else
+{
+    Console.WriteLine("Число должно быть трёхзначным");
+}
+
