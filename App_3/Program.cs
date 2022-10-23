d0d118b415b815612b94f33012ac3fef7da7d9e6
Nikita Belov <lolvvto@yandex.ru>
2022-10-23 13:00:32 +0700

    Add App 3

---
+++ App_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+if (number > 0 && number < 8)
+{
+    if (number > 0 && number < 6)
+    {
+        Console.WriteLine("Будний день");
+    }
+
+    if (number == 6 || number == 7)
+    {
+    Console.WriteLine("Выходной день");
+    }
+}
+else
+{
+    Console.WriteLine("Дня недели с таким индексом не существует");
+}
